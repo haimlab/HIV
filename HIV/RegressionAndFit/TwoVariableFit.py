dd76d0d895b4652d3f967237699ecfbb469252fd
r2dong <r2dong@yahoo.com>
2018-06-01 17:31:39 -0500

    two variable fit completed and running

--- HIV/RegressionAndFit/TwoVariableFit.py
+++ HIV/RegressionAndFit/TwoVariableFit.py
@@ -15,11 +15,13 @@ THRESHOLD = 0.001
 DT = 1
 POS_START = 2
 DIGITS_PATTERN = re.compile('\d+')
+TIME_THRESH = 380
+ZERO_VAL_STRING = '0.001'
 
 # compute proximal positions ahead of time
 proxPosDict = {} # <int, [int pos, float dist]>
 
-class timePoints:
+class TimePoint:
     def __init__(self, dT):
         self.dT = dT
         self.prePos = {}
@@ -42,10 +44,23 @@ def func(X, a, b):
 def parsePositionIndex(row):
     indices = {}
     for i in range(POS_START, len(row), 2):
-        pos = int(digitsPattern.match(row[i]).group(0))
-        indices[i] = pos # index -> vn
+        if row[i].strip() != '':
+            pos = int(DIGITS_PATTERN.match(row[i]).group(0))
+            indices[i] = pos # index -> vn
     return indices
 
+# if the string represent a number, convert it to 0 if it equals 0.001 and 1
+# otherwise, raise an exception if myString is not float
+def binConvert(myString):
+    if not myString.replace('.', '').replace('E', '').replace('-', '').isdigit():
+        raise Exception('The string is not float format')
+    if myString == ZERO_VAL_STRING:
+        return 0
+    else:
+        return 1
+
+
+# parse time points and binary convert volatility values
 def parseTimePoints():
     with open(testSetFileName, 'r') as testSetFile:
         isFirstRow = True
@@ -56,10 +71,14 @@ def parseTimePoints():
                 allTimePoints = [] # list of time point object
                 isFirstRow = False
                 continue
+            if len(row) == 0:
+                continue
             timePoint = TimePoint(int(row[DT]))
             for key in posIndices:
-                timePoint.prePos[posIndices[key]] = row[key + 1]
-                timePoint.prePos[posIndices[key]] = row[key]
+                timePoint.prePos[posIndices[key]] = binConvert(row[key + 1])
+                timePoint.postPos[posIndices[key]] = binConvert(row[key])
+            if timePoint.dT < TIME_THRESH:
+                allTimePoints.append(timePoint)
     return allTimePoints
 
 # read a distance matrix file, 
@@ -69,24 +88,31 @@ def readDistMatrix():
         reader = csv.reader(matrixFile)
         indToPosDict = {}
         adjList = {}
-        colInd = 0
+        colInd = 1
         for line in reader:
             if isFirstRow:
                 for i in range(1, len(line)):
                     indToPosDict[i] = int(line[i])
                 isFirstRow = False
-                for key in posToIndDict: # initialize adjacency list
-                    adjList[key] = {}
+                for key in indToPosDict: # initialize adjacency list
+                    adjList[indToPosDict[key]] = {}
                 continue
             for i in range(1, len(line)):
-                adjList[indToPosDict[colInd]][indToPosDict[i]] = float[row[i]]
-                adjList[indToPosDict[i]][indToPosDict[colInd]] = float[row[i]]
+                if line[i].strip() != '':
+                    adjList[indToPosDict[colInd]][indToPosDict[i]] = float(line[i])
+                    adjList[indToPosDict[i]][indToPosDict[colInd]] = float(line[i])
             colInd += 1
-        return [adjList, indToPosDict]
+        return adjList, indToPosDict
 
 # diff functions
 def spatialDiff(pValue):
-    return 2 / (1 + math.exp(-(pValue  - 2.5) * 0.3)) - 1
+    if pValue == 0:
+        pValue = 10e-9
+    pValue = -math.log(pValue, 10)
+    result = 2 / (1 + math.exp(-(pValue  - 2.5) * 0.3)) - 1
+    if result < 0:
+        result = 0
+    return result
 
 # return the temporal coefficient of a specific 
 def temporalDiff(pos):
@@ -108,7 +134,9 @@ def temporalDiff(pos):
         return 0
     elif pos == 327:
         return 0
-    elif pos == 330:
+    elif pos == 332:
+        return 0.35
+    elif pos == 330 or pos == 334:
         return 0.03
     elif pos == 415:
         return 0.03
@@ -116,7 +144,7 @@ def temporalDiff(pos):
         return 0
     elif pos == 519:
         return 0.003
-    elif pos == 770:
+    elif pos == 779:
         return 0.01
     else:
         raise Exception("not yet implemented")
@@ -129,7 +157,7 @@ if __name__ == "__main__":
         outFile.write("Position,Temporal Persistance Weight,Spatial Persistance Weight\n")
     
     # parse adjList for pValue distance
-    [adjList, indToPosDict] = readDistMatrix()
+    adjList, indToPosDict = readDistMatrix()
     
     # parse all time points
     allTimePoints = parseTimePoints()
@@ -141,34 +169,37 @@ if __name__ == "__main__":
             allPos[key] = Position(key)
     for key in allPos:
         for proxKey in adjList[key]:
-            if adjList[key][adjKey] < THRESHOLD:
-                allPos[key].proxPos.append(adjKey)
+            if adjList[key][proxKey] < THRESHOLD:
+                allPos[key].proxPos.append(proxKey)
     
     # compute independent variables for each position
-    for pos in allPos:
+    for posKey in allPos:
         x = []
         y = []
-        z = []        
+        z = []
         for tp in allTimePoints:
-            for posKey in allPos:
-                # calculate next z
-                z.append(tp.postPos.get(posKey))
-                # calculate next y
-                sigmaProx = 0.0
-                for prox in allPos[posKey].proxPos:
-                    sigmaProx += tp.prePos[prox] * (1 / tp.dT) * temporalDiff(prox) * (1 / spatialDiff(adjList[prox][posKey]))
-                y.append(sigmaProx)
-                # calculate next x
-                x.append(tp.prePos[posKey] * (1 / tp.dT) * temporalDiff(posKey))
-                
-                # initial guesses for a, b:
-                # p0 = 1, 2
-                
-                # compute fit parameters and write to file output   
-                fitResult = curve_fit(func, (x,y), z, p0)
-                with open(outFileName, 'a') as outFile:
-                    nextLine = ""
-                    nextLine += str(pos.posNum)
-                    nextLine += "," + str(fixResult[0][0])
-                    nextLine += "," + str(fixResult[0][1]) + "\n"
-                    outFile.write(nextLine)
+            # calculate next z
+            z.append(tp.postPos.get(posKey))
+            # calculate next y
+            sigmaProx = 0.0
+            for prox in allPos[posKey].proxPos:
+                sigmaProx += tp.prePos[prox] * (1 / tp.dT) * temporalDiff(prox) * spatialDiff(adjList[prox][posKey])
+            y.append(sigmaProx)
+            # calculate next x
+            x.append(tp.prePos[posKey] * (1 / tp.dT) * temporalDiff(posKey))
+            
+        # initial guesses for a, b:
+        # p0 = 1, 2
+            
+        # compute fit parameters and write to file output   
+        print(len(x))
+        print(len(y))
+        print(len(z))
+        
+        fitResult = curve_fit(func, (x,y), z)
+        with open(outFileName, 'a') as outFile:
+            nextLine = ""
+            nextLine += str(posKey)
+            nextLine += "," + str(fitResult[0][0])
+            nextLine += "," + str(fitResult[0][1]) + "\n"
+            outFile.write(nextLine)
