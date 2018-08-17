94ef260d8faf1fd9349eb718e83cd07fe4cff46b
Dong <rdong6@healthcare.uiowa.edu>
2018-08-17 10:53:14 -0500

    query verified to be working

--- HIV/Profile_Matching/query.py
+++ HIV/Profile_Matching/query.py
@@ -1,6 +1,12 @@
 import sys
+import csv
 from constants import AminoAcid
 from weighted_fit import get_all_profiles
+
+
+# script level constants
+HEADER_ROW = ['', 'stdev', 'Z', 'N', 'T', 'S', 'D', 'E', 'K', 'R', 'H', 'Y',
+                  'Q', 'I', 'L', 'V', 'A', 'C', 'F', 'G', 'M', 'P', 'W']
 
 
 class QueryInput:
@@ -35,57 +41,97 @@ queryProfile = {
 }
 queryInput = QueryInput(295, queryProfile)
 
-
+# TODO, refactor QueryResult and QueryInput into one class
 class QueryResult:
-    def __init__(self, position):
+    def __init__(self, position, input):
+        self.input = input
         self.position = position
-        self.results = {}  # {(clade, country) -> {amino acid -> year}}
+        self.results = {}  # {(clade, region) -> {amino acid -> (year, fit object)}}
+        self.scores = {}  # {(clade, region) -> stdev of corresponding profile), for writing to file specifically
+        self.best_fit_clade = None
+        self.best_fit_region = None
 
-    def add_result(self, clade, region, aminoAcid, year):
+    def add_result(self, clade, region, aminoAcid, year, fit):
         try:
-            self.results[(clade, region)][aminoAcid] = year
+            self.results[(clade, region)][aminoAcid] = year, fit
         except KeyError:
-            self.results[(clade, region)] = {aminoAcid: year}
+            self.results[(clade, region)] = {aminoAcid: (year, fit)}
 
-    def calc_stdev_(self, profile):
+    def calc_stdev(self, profile):
 
         # calculate average
         sum = 0
         for aminoAcid in profile:
-            sum += profile[aminoAcid]
+            sum += profile[aminoAcid][0]
         mean = sum / len(profile)
 
         # calculate standard deviation
         sum = 0
         for aminoAcid in profile:
-            sum += (profile[aminoAcid] - mean) ** 2
+            sum += (profile[aminoAcid][0] - mean) ** 2
         return (sum / len(profile)) ** .5
 
     def find_best_match(self):
-        best_fit_clade = None
-        best_fit_region = None
         score = sys.float_info.max
         for clade, region in self.results:
             profileDict = self.results[(clade, region)]
-            cur_score = self.calc_stdev_(profileDict)
+            cur_score = self.calc_stdev(profileDict)
+            self.scores[(clade, region)] = cur_score
             if cur_score < score:
                 score = cur_score
-                best_fit_clade = clade
-                best_fit_region = region
-        return best_fit_clade, best_fit_region
+                self.best_fit_clade = clade
+                self.best_fit_region = region
+        return self.best_fit_clade, self.best_fit_region
 
+
+    # write intermediate results for verfication purposes
+    def write_intermediate_results(self, fileName):
+        with open(fileName, 'w') as outFile:
+            writer = csv.writer(outFile, lineterminator='\n')
+
+            # write header
+            writer.writerow(HEADER_ROW)
+
+            # write query
+            queryRow = ['query', '']
+            for aminoAcid in AminoAcid:
+                queryRow.append(self.input.profile[aminoAcid])
+            writer.writerow(queryRow)
+
+            # writer calculated year and stdev using fit equations
+            for clade, region in self.results:
+
+                # write years calculated using fit
+                years = [clade.value + ", " + region.value, self.scores[clade, region]]
+                slopes = ['slope', '']
+                y_intercepts = ['y_intercept', '']
+                r_vals = ['r', '']
+                profile = self.results[(clade, region)]
+                for aminoAcid in AminoAcid:
+                    years.append(profile[aminoAcid][0])
+                    slopes.append(profile[aminoAcid][1].slope)
+                    y_intercepts.append(profile[aminoAcid][1].y_intercept)
+                    r_vals.append(profile[aminoAcid][1].r)
+                writer.writerow(years)
+                writer.writerow(slopes)
+                writer.writerow(y_intercepts)
+                writer.writerow(r_vals)
+                writer.writerow([])
 
 def query(queryProfile, allProfiles):
-    result = QueryResult(queryProfile.position)
+    result = QueryResult(queryProfile.position, queryProfile)
     allProfiles = allProfiles.filter(position=queryProfile.position)
     for aminoAcid in queryProfile.profile:
-        allProfiles = allProfiles.filter(aminoAcid=aminoAcid)
-        for p in allProfiles.profiles:
-            result.add_result(p.clade, p.region, aminoAcid, p.fit.calcYear(queryProfile.profile[aminoAcid]))
-    return result.find_best_match()
+        subProfiles = allProfiles.filter(aminoAcid=aminoAcid)
+        for p in subProfiles.profiles:
+            result.add_result(p.clade, p.region, aminoAcid, p.fit.calcYear(queryProfile.profile[aminoAcid]), p.fit)
+    result.find_best_match()
+    return result
 
 
 if __name__ == '__main__':
     allProfiles = get_all_profiles('C:\\Users\\rdong6\\Desktop\\Weighted Fit Test Data\\Processed')
     result = query(queryInput, allProfiles)
-    print(result)
+    print(result.best_fit_clade)
+    print(result.best_fit_region)
+    result.write_intermediate_results('C:\\Users\\rdong6\\Desktop\\intermediate_results.csv')
