0eebc07c8142e1376cfe8f83bc0470c0e7e338d0
Dong <rdong6@healthcare.uiowa.edu>
2018-08-17 17:00:26 -0500

    completed writing temporary results for fine tuning

--- HIV/Profile_Matching/query.py
+++ HIV/Profile_Matching/query.py
@@ -1,18 +1,10 @@
 import sys
 import csv
 from constants import AminoAcid
+from constants import Clade
+from constants import Region
 from weighted_fit import get_all_profiles
-
-
-# script level constants
-HEADER_ROW = ['', 'stdev', 'Z', 'N', 'T', 'S', 'D', 'E', 'K', 'R', 'H', 'Y',
-                  'Q', 'I', 'L', 'V', 'A', 'C', 'F', 'G', 'M', 'P', 'W']
-
-
-class QueryInput:
-    def __init__(self, position, profile):
-        self.position = position
-        self.profile = profile  # mapping from Amino Acid to percentage
+from weighted_fit import calcYear
 
 
 # temporary hard-coded inputs
@@ -39,43 +31,60 @@ queryProfile = {
     AminoAcid('P'): 0,
     AminoAcid('W'): 0,
 }
-queryInput = QueryInput(295, queryProfile)
 
-# TODO, refactor QueryResult and QueryInput into one class
-class QueryResult:
-    def __init__(self, position, input):
-        self.input = input
+
+class QueryInput:
+    def __init__(self, profile, position, year_range, clade, region):
+        self.profile = profile
         self.position = position
-        self.results = {}  # {(clade, region) -> {amino acid -> (year, fit object)}}
+        self.year_range = year_range
+        self.clade = clade
+        self.region = region
+
+
+class Query:
+    def __init__(self, query_input, all_profiles):
+        self.all_profiles = all_profiles
+        self.input = query_input
+        self.results = {}  # {(clade, region) -> {amino acid -> year}}
         self.scores = {}  # {(clade, region) -> stdev of corresponding profile), for writing to file specifically
+        self.fits = {}  # {(clade, region) -> {amino acid -> fit object}}
         self.best_fit_clade = None
         self.best_fit_region = None
 
+        all_profiles = all_profiles.filter(position=self.input.position)
+        for aminoAcid in self.input.profile:
+            sub_profiles = all_profiles.filter(aminoAcid=aminoAcid)
+            for p in sub_profiles.profiles:
+                self.add_result(p.clade, p.region, aminoAcid,
+                                p.fit.calcYear(queryProfile[aminoAcid],
+                                calcYear(self.input.year_range)), p.fit)
+        self.find_best_match()
+
     def add_result(self, clade, region, aminoAcid, year, fit):
         try:
-            self.results[(clade, region)][aminoAcid] = year, fit
+            self.results[(clade, region)][aminoAcid] = year
+            self.fits[(clade, region)][aminoAcid] = fit
         except KeyError:
-            self.results[(clade, region)] = {aminoAcid: (year, fit)}
-
-    def calc_stdev(self, profile):
-
-        # calculate average
-        sum = 0
-        for aminoAcid in profile:
-            sum += profile[aminoAcid][0]
-        mean = sum / len(profile)
-
-        # calculate standard deviation
-        sum = 0
-        for aminoAcid in profile:
-            sum += (profile[aminoAcid][0] - mean) ** 2
-        return (sum / len(profile)) ** .5
+            self.results[(clade, region)] = {aminoAcid: year}
+            self.fits[(clade, region)] = {aminoAcid: fit}
 
     def find_best_match(self):
+
+        def calc_stdev(profile):
+            sum = 0
+            for aminoAcid in profile:
+                sum += profile[aminoAcid]
+            mean = sum / len(profile)
+            sum = 0
+            for aminoAcid in profile:
+                sum += (profile[aminoAcid] - mean) ** 2
+            return (sum / len(profile)) ** .5
+
         score = sys.float_info.max
         for clade, region in self.results:
             profileDict = self.results[(clade, region)]
-            cur_score = self.calc_stdev(profileDict)
+            cur_score = calc_stdev(profileDict)
             self.scores[(clade, region)] = cur_score
             if cur_score < score:
                 score = cur_score
@@ -86,52 +95,79 @@ class QueryResult:
 
     # write intermediate results for verfication purposes
     def write_intermediate_results(self, fileName):
+        all_rows = []
+        amino_acids_list = []
+        for aminoAcid in AminoAcid:
+            amino_acids_list.append(aminoAcid.value)
+
+        # query rows
+        all_rows.append(['position:', str(self.input.position)])
+        all_rows.append(['clade:', self.input.clade.value])
+        all_rows.append(['region:', self.input.region.value])
+        all_rows.append(['period', self.input.year_range])
+        all_rows.append(['', ''] + amino_acids_list)
+        query_row = ['query', '']
+        for aminoAcid in AminoAcid:
+            query_row.append(self.input.profile[aminoAcid])
+        all_rows.append(query_row)
+        all_rows.append([])
+
+        # calculated year, stdev, and fit parameters
+        all_rows.append(['', 'stdev'] + amino_acids_list)
+        for clade, region in self.results:
+            row_collection = [
+                [clade.value + ", " + region.value, self.scores[clade, region]],
+                ['slope', ''],
+                ['y_intercept', ''],
+                ['r', ''],
+                []
+            ]
+            for aminoAcid in AminoAcid:
+                row_collection[0].append(self.results[(clade, region)][aminoAcid])
+                row_collection[1].append(self.fits[(clade, region)][aminoAcid].slope)
+                row_collection[2].append(self.fits[(clade, region)][aminoAcid].y_intercept)
+                row_collection[3].append(self.fits[(clade, region)][aminoAcid].r)
+            for row in row_collection:
+                all_rows.append(row)
+            all_rows.append([])
+
+        # write all other profiles that shared the same period with query
+        period = calcYear(self.input.year_range)
+        all_other_profiles = {}  # {(clade, region) -> {AminoAcid -> percentage}}
+        for profile in self.all_profiles.profiles:
+            key = profile.clade, profile.region
+            amino_acid = profile.aminoAcid
+            percentage = None
+            for i in range(0, len(profile.years)):
+                if profile.years[i] == period:
+                    percentage = profile.distr[i]
+                    break
+            try:
+                all_other_profiles[key][amino_acid] = percentage
+            except KeyError:
+                all_other_profiles[key] = {amino_acid: percentage}
+
+        # put all other profiles into the list of rows to be written
+        all_rows.append([])
+        all_rows.append(['other profiles in the same period'])
+        all_rows.append(['', ''] + amino_acids_list)
+        for clade, region in all_other_profiles:
+            profile = all_other_profiles[(clade, region)]
+            percentage_row = [clade.value + ", " + region.value, '']
+            for amino_acid in AminoAcid:
+                percentage_row.append(profile[amino_acid])
+            all_rows.append(percentage_row)
+
+        # write to output file
         with open(fileName, 'w') as outFile:
             writer = csv.writer(outFile, lineterminator='\n')
-
-            # write header
-            writer.writerow(HEADER_ROW)
-
-            # write query
-            queryRow = ['query', '']
-            for aminoAcid in AminoAcid:
-                queryRow.append(self.input.profile[aminoAcid])
-            writer.writerow(queryRow)
-
-            # writer calculated year and stdev using fit equations
-            for clade, region in self.results:
-
-                # write years calculated using fit
-                years = [clade.value + ", " + region.value, self.scores[clade, region]]
-                slopes = ['slope', '']
-                y_intercepts = ['y_intercept', '']
-                r_vals = ['r', '']
-                profile = self.results[(clade, region)]
-                for aminoAcid in AminoAcid:
-                    years.append(profile[aminoAcid][0])
-                    slopes.append(profile[aminoAcid][1].slope)
-                    y_intercepts.append(profile[aminoAcid][1].y_intercept)
-                    r_vals.append(profile[aminoAcid][1].r)
-                writer.writerow(years)
-                writer.writerow(slopes)
-                writer.writerow(y_intercepts)
-                writer.writerow(r_vals)
-                writer.writerow([])
-
-def query(queryProfile, allProfiles):
-    result = QueryResult(queryProfile.position, queryProfile)
-    allProfiles = allProfiles.filter(position=queryProfile.position)
-    for aminoAcid in queryProfile.profile:
-        subProfiles = allProfiles.filter(aminoAcid=aminoAcid)
-        for p in subProfiles.profiles:
-            result.add_result(p.clade, p.region, aminoAcid, p.fit.calcYear(queryProfile.profile[aminoAcid]), p.fit)
-    result.find_best_match()
-    return result
+            writer.writerows(all_rows)
 
 
 if __name__ == '__main__':
-    allProfiles = get_all_profiles('C:\\Users\\rdong6\\Desktop\\Weighted Fit Test Data\\Processed')
-    result = query(queryInput, allProfiles)
-    print(result.best_fit_clade)
-    print(result.best_fit_region)
-    result.write_intermediate_results('C:\\Users\\rdong6\\Desktop\\intermediate_results.csv')
+    allProfiles = get_all_profiles(sys.argv[1])
+    input = QueryInput(queryProfile, 295, '[2000, 2004]', Clade.B, Region.EU)
+    query = Query(input, allProfiles)
+    print(query.best_fit_clade)
+    print(query.best_fit_region)
+    query.write_intermediate_results(sys.argv[2])
--- HIV/Profile_Matching/weighted_fit.py
+++ HIV/Profile_Matching/weighted_fit.py
@@ -21,9 +21,14 @@ class FitResult:
         self.r = r
 
     # calculate year based on percent
-    def calcYear(self, percent):
-        return (percent - self.y_intercept) / self.slope
-
+    def calcYear(self, percent, default_year):
+        try:
+            return (percent - self.y_intercept) / self.slope
+        except ZeroDivisionError:
+            if percent == self.y_intercept:
+                return default_year
+            else:
+                return float('inf')
 
 # object to hold a single distribution profile
 class Profile:
@@ -103,15 +108,63 @@ class AllProfiles:
             return self
 
 
+# calculate r square value of a linear regression, referencing following site
+# https://en.wikipedia.org/wiki/Coefficient_of_determination#Definitions
+def calc_r_squared(slope, y_intercept, x_data, y_data):
+
+    if len(x_data) != len(y_data):
+        raise Exception("input data size mis-match")
+
+    def average(arr):
+        total = 0
+        for i in arr:
+            total += i
+        return total / len(arr)
+
+    def sum_squares(arr):
+        avg = average(arr)
+        var = 0
+        for i in arr:
+            var += (i - avg) ** 2
+        return var
+
+    def sum_residual(slope, y_intercept, x_data, y_data):
+        total = 0
+        for x, y in zip(x_data, y_data):
+            f_i = x * slope + y_intercept
+            total += (y - f_i) ** 2
+        return total
+
+    sum_squares_tot = sum_squares(y_data)
+    sum_squares_residual = sum_residual(slope, y_intercept, x_data, y_data)
+    return 1 - sum_squares_residual / sum_squares_tot
+
+
 # take a profile and compute its weighted linear fit
 def calcFit(profile):
+
+    def checkEqual(arr):
+        for i in range(0, len(arr) - 1):
+            if arr[i] != arr[i + 1]:
+                return False
+        return True
+
     profile.remove_0_isolates()
-    sigmas = np.array([1 / n ** .5 for n in profile.numIso]) # weights
-    params, cov = curve_fit(lambda x, a, b: a * x + b, profile.years,
-                            profile.distr, sigma=sigmas, absolute_sigma=False)
-    profile.fit = FitResult(params[0], params[1], cov)
-    
-    
+
+    # to avoid float number round off errors, manually check if all data points are same
+    # and assign slope = 0, y_intercept = any data point value, and r square = 1 (perfect fit)
+    if checkEqual(profile.distr):
+        params = [0, profile.distr[0]]
+        r_squared = 1
+    else:
+        # calculate fit parameters
+        sigmas = np.array([1 / n ** .5 for n in profile.numIso]) # weights
+        params, cov = curve_fit(lambda x, a, b: a * x + b, profile.years,
+                                profile.distr, sigma=sigmas, absolute_sigma=False)
+        r_squared = calc_r_squared(params[0], params[1], profile.years, profile.distr)
+    profile.fit = FitResult(params[0], params[1], r_squared)
+
+
 # calculate year as median of the range
 # assumes input string to look like "[year1, year2]" with year1 < year2
 def calcYear(yearRange):
@@ -135,7 +188,7 @@ def read(fileName):
     allProfiles = []
     clade, region, position = parseFileName(fileName)
 
-    with open(fileName, 'r') as file:
+    with open(fileName) as file:
         reader = csv.reader(file)
 
         # read in years
@@ -210,14 +263,10 @@ def euclideanDist(p1, p2):
 #         minDistProfile.mostSimilar = p1
 #
 #
-def main():
-    fileName = sys.argv[1]
-    profiles = read(fileName)
-    for p in profiles.profiles:
-        calcFit(p)
 
-    # plot second profile for verification
-    p = profiles.profiles[2]
+def main():
+    all_profiles = get_all_profiles(sys.argv[1])
+    p = all_profiles.profiles[2]
     x = p.years
     yexact = p.distr
     y = []
@@ -232,50 +281,3 @@ def main():
 # actual main
 if __name__ == '__main__':
     main()
-#
-#
-# # tests and scratches
-# if __name__ == '__main__':
-#
-#     profiles = read('C:\\Users\\rdong6\\Desktop\\Weighted Fit Test Data\\B_ALL_332.csv')
-#     for p in profiles:
-#         calcFit(p)
-#
-#     ## plot second profile for verification
-#     #p = profiles[0]
-#     #x = p.years
-#     #yexact = p.distr
-#     #y = []
-#     #for i in x:
-#         #y.append(p.fit.slope * i + p.fit.y_intercept)
-#     #pylab.plot(x, yexact, 'o', label='Exact')
-#     #pylab.plot(x, y, label='weighted fit')
-#     #pylab.legend(loc='upper right')
-#     #pylab.show()
-#
-#     # verifiy the transform functions
-#     L = 100
-#     k = -1.2
-#     x_0 = 5
-#     logiFunc = lambda x: L / (1 + exp(k * (x - x_0))) # logistic function
-#     logConvert = lambda x: 0 if x == 0 else log10(x) + 1 # log convert
-#     combinedFunc = lambda x: logiFunc(logConvert(x)) # combined logistic and log
-#     # use B NA / all B
-#     pylab.figure()
-#     for p in profiles:
-#         y = [combinedFunc(i) for i in p.distr]
-#         pylab.plot(p.distr, y, 'o', label=p.tag())
-#     pylab.xlabel("actual")
-#     pylab.ylabel("converted")
-#     pylab.xscale('log')
-#     pylab.legend(loc='lower right')
-#     pylab.show()
-#
-#     # export as two columns
-#     with open('C:\\Users\\rdong6\\Desktop\\out.csv', 'w') as outFile:
-#         writer = csv.writer(outFile, lineterminator='\n')
-#         writer.writerow(['actual percentage', 'converted'])
-#         for p in profiles:
-#             y = [combinedFunc(i) for i in p.distr]
-#             for a, b in zip(y, p.distr):
-#                 writer.writerow([b, a])
