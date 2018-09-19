0c07b944efdb6c8f54f5e5824e05535365d2dc47
Dong <rdong6@healthcare.uiowa.edu>
2018-09-19 18:14:08 -0500

    working, and added cmdln input interface

--- src/HIV/Profile_Matching/prediction.py
+++ src/HIV/Profile_Matching/prediction.py
@@ -1,7 +1,9 @@
 from file_parse import get_all_dynamic_profiles
 from weighted_fit import calcFit
 from constants import AminoAcid, Clade, Region
-
+import argparse
+from os.path import join
+from csv import writer
 
 def predict(all_profiles, clade, region, position, year):
     res = {}
@@ -15,23 +17,37 @@ def predict(all_profiles, clade, region, position, year):
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', nargs='+', dest='positions', type=int, required=True)
+    parser.add_argument('-c', nargs='+', dest='clades', type=str, required=True)
+    parser.add_argument('-y', nargs='+', dest='years', type=int, required=True)
+    parser.add_argument('-r', nargs='+', dest='regions', type=str, required=True)
+    parser.add_argument('-o', dest='out_dir', type=str, required=True)
+    cmd_args = parser.parse_args()
+    positions = cmd_args.positions
+    clades = [Clade(c) for c in cmd_args.clades]
+    years = cmd_args.years
+    regions = [Region(r) for r in cmd_args.regions]
+    out_dir = cmd_args.out_dir
+
     all_profiles = get_all_dynamic_profiles()
     for p in all_profiles.get_all_profiles():
         calcFit(p)
 
-    years = [2018, 2028, 2038, 2048]
-    clades = [Clade.AE, Clade.B, Clade.C]
-    positions = [295, 332, 339, 392, 448]
+    rows = []
+    rows.append(['Clade', 'Region', 'Position', 'Year'] + [str(aa.value) for aa in AminoAcid])
     for c in clades:
-        for pos in positions:
-            for y in years:
-                res = predict(all_profiles, c, Region.ALL, pos, y)
-                print(c)
-                print(pos)
-                print(y)
-                print(res)
-        print()
-        print()
+        for r in regions:
+            for p in positions:
+                for y in years:
+                    row = [str(c.value), str(r.value), str(p), str(y)]
+                    res = predict(all_profiles, c, r, p, y)
+                    row += [str(res[aa]) for aa in AminoAcid]
+                    rows.append(row)
+
+    with open(join(out_dir, 'out.csv'), 'w') as out_file:
+        w = writer(out_file, lineterminator='\n')
+        w.writerows(rows)
 
 
 if __name__ == '__main__':
