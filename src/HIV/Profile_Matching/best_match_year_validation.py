795627e40a106165106e8734a96821032a409d17
r2dong <r2dong@yahoo.com>
2018-09-21 22:09:46 -0500

    best match year validation finished but not tested
    had to make this commit as pycharm kept doing this 'revert from disk'
    thing. It overrites whatever changes made and does not even allow undo.

--- src/HIV/Profile_Matching/best_match_year_validation.py
+++ src/HIV/Profile_Matching/best_match_year_validation.py
@@ -22,16 +22,16 @@ def write_validation_results(query, out_file_name, y2predict):
         writer.writerow([''])
 
         # write best mathcing information
-        clade, region = query.find_diff_region_best_match()
+        clade, region, _, _ = query.find_diff_region_best_match()
         writer.writerow(['best matching information'])
         writer.writerow(['region:', region.value])
         writer.writerow(['Profile'] + [aa.value for aa in constants.AminoAcid])
         all_prof = get_all_dynamic_profiles().filter(clade=query.input.clade, region=query.input.region,
-                                                     position=query.input.position).get_all_profiles()
+                                                     position=query.input.position)
         prof = {}
         for aa in constants.AminoAcid:
             cur_prof = all_prof.filter(aminoAcid=aa)
-            if len(cur_prof.get_all_profiles()):
+            if len(cur_prof.get_all_profiles()) != 1:
                 raise Exception('something is wrong')
             p = cur_prof.get_all_profiles()[0]
             prof[aa] = p.get_distr(calcYear(query.input.year_range))
@@ -43,11 +43,11 @@ def write_validation_results(query, out_file_name, y2predict):
         writer.writerow(['year:', str(y2predict)])
         writer.writerow(['Profile'] + [aa.value for aa in constants.AminoAcid])
         all_prof = get_all_dynamic_profiles().filter(clade=query.input.clade, region=query.input.region,
-                                                     position=query.input.position).get_all_profiles()
+                                                     position=query.input.position)
         prof = {}
         for aa in constants.AminoAcid:
             cur_prof = all_prof.filter(aminoAcid=aa)
-            if len(cur_prof.get_all_profiles()):
+            if len(cur_prof.get_all_profiles()) != 1:
                 raise Exception('something is wrong')
             p = cur_prof.get_all_profiles()[0]
             prof[aa] = p.fit.calc_distr(y2predict)
@@ -55,26 +55,39 @@ def write_validation_results(query, out_file_name, y2predict):
         writer.writerow([''])
 
         # write actual profile information
-        # TODO continue here
+        writer.writerow(['measured future profile'])
+        writer.writerow(['year:', str(y2predict)])
+        writer.writerow(['region:', query.input.region.value])
+        writer.writerow(['Profile'] + [aa.value for aa in constants.AminoAcid])
+        all_prof = get_all_dynamic_profiles().filter(clade=query.input.clade, region=query.input.region,
+                                                     position=query.input.position)
+        prof = {}
+        for aa in constants.AminoAcid:
+            cur_prof = all_prof.filter(aminoAcid=aa)
+            if len(cur_prof.get_all_profiles()) != 1:
+                raise Exception('something is wrong')
+            p = cur_prof.get_all_profiles()[0]
+            prof[aa] = p.get_distr(y2predict)
+        writer.writerow([''] + [prof[aa] for aa in constants.AminoAcid])
+        writer.writerow([''])
 
 
 def main():
 
     query_inputs = [
-        QueryInput(constants.query_profile_B_EU_339_87_to_94, 339, '[1987, 1994]', constants.Clade.B, constants.Region.EU),
-        QueryInput(constants.query_profile_B_EU_392_87_to_94, 392, '[1987, 1994]', constants.Clade.B, constants.Region.EU)
+        QueryInput(constants.query_profile_B_EU_339_87_to_94, 339, '[1987, 1994]',
+                   constants.Clade.B, constants.Region.EU),
+        QueryInput(constants.query_profile_B_EU_392_87_to_94, 392, '[1987, 1994]',
+                   constants.Clade.B, constants.Region.EU)
     ]
 
-    for query_input in query_inputs:
+    for file_name, query_input in zip(sys.argv, query_inputs):
         allProfiles = get_all_dynamic_profiles()
         for p in allProfiles.get_all_profiles():
             calcFit(p)
         query = Query(query_input, allProfiles)
-        clade, region = query.find_diff_region_best_match()
-        print(clade)
-        print(region)
-        file_name = join(sys.argv[1], str(query_input.position) + '_r2=0.4.csv')
         query.write_intermediate_results(file_name)
 
+
 if __name__ == '__main__':
-    pass
+    main()
