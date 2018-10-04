5a17cf29628ae4f3dae1453e20993695760cecf6
Dong <rdong6@healthcare.uiowa.edu>
2018-10-04 11:28:40 -0500

    add test for C 295 and AE 332 filtering

--- test/HIV/Profile_Matching/profile_p_value_test.py
+++ test/HIV/Profile_Matching/profile_p_value_test.py
@@ -18,3 +18,19 @@ class TestStringMethods(unittest.TestCase):
                 p.clade() == constants.Clade.C and p.region() == constants.Region.EU or
                 p.clade() == constants.Clade.AE and p.region() == constants.Region.TH
             )
+
+    def test_select_sub_group(self):
+
+        all_prof = file_parse.get_all_static_profiles()
+        sub_prof = profile_p_value.select_sub_group(all_prof, ['C,EU', 'AE,TH'], [295, 332, 392])
+        count_C = 0
+        count_AE = 0
+        for p in sub_prof.get_all_profiles():
+            if p.clade() == constants.Clade.C:
+                self.assertTrue(p.position() != 295)
+                count_C += 1
+            if p.clade() == constants.Clade.AE:
+                self.assertTrue(p.position() != 332)
+                count_AE += 1
+        self.assertEqual(count_AE, 2)
+        self.assertEqual(count_C, 2)
