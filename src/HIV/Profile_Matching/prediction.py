09a6a42f7bdf0899a2e7d6c5a6313ed8f524361d
Dong <rdong6@healthcare.uiowa.edu>
2018-10-04 11:27:31 -0500

    support prediction with selected data points

--- src/HIV/Profile_Matching/prediction.py
+++ src/HIV/Profile_Matching/prediction.py
@@ -31,7 +31,7 @@ def main():
     parser.add_argument('-c', nargs='+', dest='clades', type=str, required=True)
     parser.add_argument('-y', nargs='+', dest='years', type=float, required=True)
     parser.add_argument('-r', nargs='+', dest='regions', type=str, required=True)
-    parser.add_argument('-t', dest='omit_last', type=bool, required=True)
+    parser.add_argument('-t', nargs='+', dest='omit', type=float, required=True)
     parser.add_argument('-o', dest='out_dir', type=str, required=True)
     cmd_args = parser.parse_args()
     positions = cmd_args.positions
@@ -42,10 +42,7 @@ def main():
 
     all_profiles = get_all_dynamic_profiles()
     for p in all_profiles.get_all_profiles():
-        if cmd_args.omit_last:
-            calcFit(p, skip_tail=True)
-        else:
-            calcFit(p, skip_tail=False)
+        calcFit(p, skip=cmd_args.omit)
 
     rows_predicted = []
     rows_actual = []
--- src/HIV/Profile_Matching/weighted_fit.py
+++ src/HIV/Profile_Matching/weighted_fit.py
@@ -2,6 +2,7 @@ import numpy as np
 import pylab
 from scipy.optimize import curve_fit
 from file_parse import get_all_dynamic_profiles
+from copy import copy
 
 
 class FitResult:
@@ -62,7 +63,7 @@ def calc_r_squared(slope, y_intercept, x_data, y_data):
 
 
 # take a profile and compute its weighted linear fit
-def calcFit(profile, skip_tail=False):
+def calcFit(profile, skip=[]):
 
     def checkEqual(arr):
         for i in range(0, len(arr) - 1):
@@ -70,13 +71,18 @@ def calcFit(profile, skip_tail=False):
                 return False
         return True
 
-    if skip_tail:
-        distr_tail = profile.distr[-1]
-        num_iso_tail = profile.numIso[-1]
-        years_tail = profile.years[-1]
-        del profile.distr[-1]
-        del profile.numIso[-1]
-        del profile.years[-1]
+
+    distr_copy = copy(profile.distr)
+    num_iso_copy = copy(profile.distr)
+    years_copy = copy(profile.years)
+    i = 0
+    while i < len(profile.years):
+        if profile.years[i] in skip:
+            del profile.distr[i]
+            del profile.numIso[i]
+            del profile.years[i]
+            continue
+        i += 1
 
     profile.remove_0_isolates()
 
@@ -91,15 +97,14 @@ def calcFit(profile, skip_tail=False):
         params, cov = curve_fit(lambda x, a, b: a * x + b, profile.years,
                                 profile.distr, sigma=sigmas, absolute_sigma=False)
         r_squared = calc_r_squared(params[0], params[1], profile.years, profile.distr)
+        print(profile.years)
+        print(profile.distr)
     profile.fit = FitResult(params[0], params[1], r_squared)
 
-    if skip_tail:
-        # noinspection PyUnboundLocalVariable
-        profile.distr.append(distr_tail)
-        # noinspection PyUnboundLocalVariable
-        profile.numIso.append(num_iso_tail)
-        # noinspection PyUnboundLocalVariable
-        profile.years.append(years_tail)
+    # restore modifed sequences
+    profile.distr = distr_copy
+    profile.iso_copy = num_iso_copy
+    profile.years_copy = years_copy
 
 
 def main():
