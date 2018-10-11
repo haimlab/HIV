4917258bdc6a274694d2d579e0b7a2c673ad6355
Rentian Dong <r2dong@yahoo.com>
2018-10-11 01:30:40 -0500

    clean up redundant code

--- src/HIV/Profile_Matching/profile_p_value.py
+++ src/HIV/Profile_Matching/profile_p_value.py
@@ -4,7 +4,6 @@ from scipy.cluster.vq import kmeans
 import constants
 from constants import FilterProperties
 from argparse import ArgumentParser
-from copy import deepcopy
 
 
 # output type {prop_type - {amino acid -> percentage}
@@ -82,14 +81,6 @@ def avg_centroid(centroids):
     return sum(dist_list) / len(dist_list)
 
 
-def avg_centroid_2(cur_centroid, all_centroids):
-    dist = []
-    for c in all_centroids:
-        if c != cur_centroid:
-            dist.append(euc_dist(cur_centroid, c))
-    return sum(dist)/len(dist)
-
-
 # calculates the ratio of average distance between all centroids and average distance between all centriods
 # and their sub-regions.
 def ratio(profs, prop_type):
@@ -98,99 +89,6 @@ def ratio(profs, prop_type):
     avg_dist_sub_group = sum([distances[key] for key in distances]) / len(distances)
     avg_centroid_dist = avg_centroid(all_centroids)  # average distance between all centroids
     return avg_centroid_dist / avg_dist_sub_group
-
-
-def calc_p(std, shuffled):
-    n = 0
-    d = 0
-    for i in shuffled:
-        if i > std:
-            n += 1
-        else:
-            d += 1
-    return n / d
-
-
-def clade_and_region_specificty_helper(all_profiles, positions, cur_pos, pairs):
-
-    positional_centroids = calc_all_centroids(all_profiles, FilterProperties.POSITION)
-    # remove postions not wanted
-    for p in positional_centroids:
-        if p not in positions:
-            del positional_centroids[p]
-
-    pos_sub = all_profiles.filter(cur_pos)
-    pos_centroid = calc_centroid(pos_sub)
-
-    # convert string pairs into constants
-    pairs_2 = [s.split(',') for s in pairs]
-    flat_pairs = [(constants.Clade(c), constants.Region(r)) for [c, r] in pairs_2]
-    clades = set()
-    regions = set()
-    for clade, region in flat_pairs:
-        clades.add(clade)
-        regions.add(region)
-
-    distances = []
-    for clade in clades:
-        for region in regions:
-            cr_sub = pos_sub.filter(clade).filter(region)
-            try:
-                p = cr_sub.get_only_profile()
-            except Exception as e:
-                if str(e) == 'cannot find single profile':
-                    # since C 295 and AE 332 are always discarded, after shuffling,
-                    # some other clade position combination will guarentee to be missing
-                    # so we cannot be certain which ones will be available but to catch
-                    # all these exceptions and ignore them
-                    continue
-                    # if cur_pos == 295 and clade == constants.Clade.C:
-                    #     continue
-                    # elif cur_pos == 332 and clade == constants.Clade.AE:
-                    #     continue
-                    # else:
-                    #     raise e
-                else:
-                    raise e
-            profile_centroid = {}
-            for aa in constants.AminoAcid:
-                profile_centroid[aa] = p.get_distr(aa)
-            distances.append(euc_dist(pos_centroid, profile_centroid))
-    try:
-        avg_dist_within = sum(distances) / len(distances)
-    except ZeroDivisionError:
-        return None
-
-    cent_list = [positional_centroids[i] for i in positional_centroids]
-    avg_dist_without = avg_centroid_2(pos_centroid, cent_list)
-
-    return avg_dist_within / avg_dist_without
-
-
-def clade_and_region_specificity(num_shuffle, all_profiles, positions, pairs):
-
-    for p in positions:
-        std_rat = clade_and_region_specificty_helper(all_profiles, positions, p, pairs)
-        num_above = 0
-        num_below = 0
-        num_skipped = 0
-        for _ in range(0, num_shuffle):
-            shuffled = all_profiles.shuffle(FilterProperties.POSITION)
-            rand_rat = clade_and_region_specificty_helper(shuffled, positions, p, pairs)
-            if rand_rat is None:
-                num_skipped += 1
-                continue
-            if rand_rat > std_rat:
-                num_above += 1
-            elif rand_rat < std_rat:
-                num_below += 1
-
-        print(f'skipped: {num_skipped}')
-        try:
-            print(f'position: {p}, p-value: {num_below / num_above}')
-        except ZeroDivisionError:
-            print(f'position: {p}, zero div')
-            print(f'std ratio: {std_rat}')
 
 
 def clade_specificity(num_shuffle, all_profiles, positions):
@@ -211,19 +109,6 @@ def clade_specificity(num_shuffle, all_profiles, positions):
                 num_equal += 1
         print(f'position: {pos}, p-value: {num_below / num_above}')
         print(f'below: {num_below}, above: {num_above}, equal: {num_equal}')
-
-
-def pos_specificity(num_shuffle, all_profiles):
-
-    # non-shuffled ratio
-    std_rat = ratio(all_profiles, FilterProperties.POSITION)
-
-    shuffled_rat = []
-    for i in range(0, num_shuffle):
-        shuffled_prof = all_profiles.shuffle(FilterProperties.POSITION)
-        shuffled_rat.append(ratio(shuffled_prof, FilterProperties.POSITION))
-
-    print('p-value: ' + str(calc_p(std_rat, shuffled_rat)))
 
 
 def select_sub_group(raw, clade_region_pairs, positions):
@@ -260,13 +145,11 @@ def specificity_one_round(all_profiles, group_by_type, cur_prop_val):
     cur_centroid = all_centroids[cur_prop_val]
     del all_centroids[cur_prop_val]
     dist_without = sum([euc_dist(cur_centroid, all_centroids[c]) for c in all_centroids]) / len(all_centroids)
-    # print(f'dist without: {dist_without}')
 
     # calculate distance within
     sub_profiles = all_profiles.filter(cur_prop_val)
     total = sum([euc_dist(cur_centroid, p.get_entire_distr()) for p in sub_profiles.get_all_profiles()])
     dist_within = total / len(sub_profiles.get_all_profiles())
-    # print(f'dist_within: {dist_within}')
 
     return dist_within / dist_without
 
@@ -281,8 +164,8 @@ def specificity(num_shuffle, all_profiles, group_by_type, properties_to_shuffle)
         num_below = 0
         for _ in range(0, num_shuffle):
             shuffled_profiles = all_profiles
-            for p2shuffle in properties_to_shuffle:
-                shuffled_profiles = shuffled_profiles.shuffle(p2shuffle)
+            for p in properties_to_shuffle:
+                shuffled_profiles = shuffled_profiles.shuffle(p)
             shuffled_rat = specificity_one_round(shuffled_profiles, group_by_type, prop)
             if shuffled_rat > std_rat:
                 num_above += 1
@@ -297,7 +180,7 @@ def specificity(num_shuffle, all_profiles, group_by_type, properties_to_shuffle)
 
 
 def corrected_position_specificity(num_shuffle, all_profiles):
-    specificity(num_shuffle, all_profiles, FilterProperties.POSITION, FilterProperties.POSITION)
+    specificity(num_shuffle, all_profiles, FilterProperties.POSITION, [FilterProperties.POSITION])
 
 
 def main():
@@ -321,7 +204,7 @@ def main():
     if cmd_args.type == 'clade':
         clade_specificity(cmd_args.num_shuffle, all_prof, positions)
     elif cmd_args.type == 'position':
-        pos_specificity(cmd_args.num_shuffle, all_prof)
+        corrected_position_specificity(cmd_args.num_shuffle, all_prof)
     # elif cmd_args.type == 'clade and region':
     #     clade_and_region_specificity(cmd_args.num_shuffle, all_prof, positions, cmd_args.clade_region_pairs)
     else:
