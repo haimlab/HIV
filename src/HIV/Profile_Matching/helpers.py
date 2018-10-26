6a5ae55753dd14d80095554eb4cb958d1627120f
Dong <rdong6@healthcare.uiowa.edu>
2018-10-26 10:49:56 -0500

    refactor envelopes to profile

--- src/HIV/Profile_Matching/helpers.py
+++ src/HIV/Profile_Matching/helpers.py
@@ -4,21 +4,11 @@ from file_parse import logConvert
 
 # compute profile from all given envelopes
 # i -> column index of Amino Acids to be counted
-def envelopes_to_profile(envs, i):
+def envelopes_to_profile(envs, i, log=True):
     count = {aa: 0 for aa in AminoAcid}
     for e in envs:
         count[AminoAcid(e[i])] += 1
     s = sum([count[aa] for aa in count])
     for aa in count:
-        count[aa] = logConvert(count[aa] / s)
-    return count
-
-
-def envelopes_to_profile_no_log(envs, i):
-    count = {aa: 0 for aa in AminoAcid}
-    for e in envs:
-        count[AminoAcid(e[i])] += 1
-    s = sum([count[aa] for aa in count])
-    for aa in count:
-        count[aa] = count[aa] / s
+        count[aa] = logConvert(count[aa] / s) if log else count[aa] / s
     return count
---
+++ test/HIV/Profile_Matching/helpers_test.py
@@ -0,0 +1,37 @@
+import unittest
+import constants
+import helpers
+
+
+class TestProfilePValue(unittest.TestCase):
+
+    def test_envlopes_to_profile_log(self):
+
+        sequences = \
+            ['Z'] * 10 + \
+            ['N'] * 50 + \
+            ['T'] * 1
+        prof = helpers.envelopes_to_profile(sequences, 0)
+        for aa in constants.AminoAcid:
+            if aa == constants.AminoAcid.Z:
+                self.assertAlmostEqual(0.21467016498, prof[aa])
+            elif aa == constants.AminoAcid.N:
+                self.assertAlmostEqual(0.91364016932, prof[aa])
+            else:
+                self.assertEqual(0, prof[aa])
+
+    def test_envelopes_to_profile_non_log(self):
+        sequences = \
+            ['Z'] * 10 + \
+            ['N'] * 50 + \
+            ['T'] * 1
+        prof = helpers.envelopes_to_profile(sequences, 0, log=False)
+        for aa in constants.AminoAcid:
+            if aa == constants.AminoAcid.Z:
+                self.assertAlmostEqual(0.163934426229508196721311475409836065573770491803278688524, prof[aa])
+            elif aa == constants.AminoAcid.N:
+                self.assertAlmostEqual(0.819672131147540983606557377049180327868852459016393442622, prof[aa])
+            elif aa == constants.AminoAcid.T:
+                self.assertAlmostEqual(0.016393442622950819672131147540983606557377049180327868852, prof[aa])
+            else:
+                self.assertEqual(0, prof[aa])
