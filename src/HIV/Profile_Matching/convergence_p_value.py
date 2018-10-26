6e1e1f3667777422c63f5867cdae3112d2adaf2f
Dong <rdong6@healthcare.uiowa.edu>
2018-10-26 10:16:05 -0500

    support writing groups to output

--- src/HIV/Profile_Matching/convergence_p_value.py
+++ src/HIV/Profile_Matching/convergence_p_value.py
@@ -10,7 +10,7 @@ STEP 6: Repeat the above for the second set of 100 Envs (~1993-1998).
 """
 
 from constants import AminoAcid
-from csv import reader
+from csv import reader, writer
 from random import sample
 from profile_p_value import euc_dist
 from argparse import ArgumentParser
@@ -31,6 +31,16 @@ def envelopes_to_profile(envs, i):
     s = sum([count[aa] for aa in count])
     for aa in count:
         count[aa] = logConvert(count[aa] / s)
+    return count
+
+
+def envelopes_to_profile_no_log(envs, i):
+    count = {aa: 0 for aa in AminoAcid}
+    for e in envs:
+        count[AminoAcid(e[i])] += 1
+    s = sum([count[aa] for aa in count])
+    for aa in count:
+        count[aa] = count[aa] / s
     return count
 
 
@@ -57,6 +67,17 @@ def read_nth_group(f_name, pos, n, group_size):
     return envelopes_to_profile(envs, first_row.index(str(pos)))
 
 
+def read_nth_group_no_log(f_name, pos, n, group_size):
+    with open(f_name) as f:
+        r = reader(f)
+        first_row = next(r)
+        for _ in range(0, n):
+            for _ in range(0, group_size):
+                next(r)
+        envs = [next(r) for _ in range(group_size)]
+    return envelopes_to_profile_no_log(envs, first_row.index(str(pos)))
+
+
 # get centroid of 2007 to 2015
 def get_last_p_centroid(fn, pos, start, end):
     with open(fn) as f:
@@ -73,6 +94,7 @@ def main():
     parser.add_argument('-g', dest='group_size', type=int, required=True)
     parser.add_argument('-s', dest='start', type=int, required=True)
     parser.add_argument('-e', dest='end', type=int, required=True)
+    parser.add_argument('-o', dest='out_file_name', type=str, required=True)
     cmd_args = parser.parse_args()
     file_name = join(FILE_DIR, cmd_args.file_name)
 
@@ -83,6 +105,12 @@ def main():
 
     positions = [295, 332, 339, 392, 448]
     for pos in positions:
+
+        with open(cmd_args.out_file_name, 'a') as of:
+            w = writer(of, lineterminator='\n')
+            w.writerow(['position: ' + str(pos)])
+            w.writerow([''] + [aa.value for aa in AminoAcid])
+
         print(f'position: {pos}')
         for n in range(0, int(float_info.max)):
             # step 1, get 1000 random shuffled profiles
@@ -94,6 +122,12 @@ def main():
             # step 4, distacne between first 100 historical envs with 07-15 cnetroid
             try:
                 dist_first_100 = euc_dist(read_nth_group(file_name, pos, n, cmd_args.group_size), cent_last_p)
+                prof = read_nth_group_no_log(file_name, pos, n, cmd_args.group_size)
+                with open(cmd_args.out_file_name, 'a') as of:
+                    start = str(n * cmd_args.group_size)
+                    end = str(n*cmd_args.group_size + cmd_args.group_size)
+                    w = writer(of, lineterminator='\n')
+                    w.writerow([start + ' to ' + end] + [prof[aa] for aa in AminoAcid])
             except StopIteration:
                 break
             # step 5, calculate the ratio as #distance which a random profile from step 1 to 07-15
@@ -101,6 +135,9 @@ def main():
             ratio = len(list(filter(lambda x: x > dist_first_100, distances))) / 1000
             print(f'{n * cmd_args.group_size}-{n*cmd_args.group_size + cmd_args.group_size}: {ratio}')
         print()
+
+        with open(cmd_args.out_file_name, 'a') as of:
+            of.write('\n')
 
 
 if __name__ == '__main__':
